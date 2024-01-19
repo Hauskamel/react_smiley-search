656bb7270929592e7a779a72aa67d4d6ae431773
Hauskamel <lukas.krojer@gmx.de>
2024-01-19 16:49:42 +0100

    init commit - components folder and smiley.jsx edited as well as App.jsx and main.jsx prep for smiley search

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+
+function App(smileysData) {
+
+  console.log(smileysData)
+
+  smileysData.forEach(smiley => {
+
+  })
+
+  return (
+    <>
+      <div className="search_wrapper">
+        <input className="search_field"/>
+        <ul>
+          <li>Smile‚y: {}</li>
+          <li>Smiley: Name of Smiles</li>
+          <li>Smiley: Name of Smiles</li>
+        </ul>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+fetch("../node_modules/emoji.json/emoji.json")
+    .then((response) => response.json())
+    .then((json) => {
+        ReactDOM.createRoot(document.getElementById('root')).render(
+            <React.StrictMode>
+                <App data={json}/>
+            </React.StrictMode>
+        );
+    });
